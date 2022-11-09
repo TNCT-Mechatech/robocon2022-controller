c89374e34dc591af938c2acdf8e202b500ac5d80
testusuke <silverbro007@icloud.com>
2022-11-09 22:13:21 +0900

    feat: created logic

---
+++ main.cpp
@@ -0,0 +1,387 @@
+#include "AnalogIn.h"
+#include "BufferedSerial.h"
+#include "DigitalIn.h"
+#include "InterruptIn.h"
+#include "PinNamesTypes.h"
+#include "Timer.h"
+#include "stdint.h"
+#include "mbed.h"
+#include <cstdint>
+#include <cstdio>
+
+
+////////////////////////////////////
+//  Private Defenition
+//  Left cross key
+#define LC_UP PC_10
+#define LC_LEFT PC_12
+#define LC_RIGHT PD_2
+#define LC_DOWN PC_11
+//  Right cross key
+#define RC_UP PB_3
+#define RC_LEFT PB_4
+#define RC_RIGHT PB_5
+#define RC_DOWN PA_10
+
+//  Left button
+#define LB_1 PB_14
+#define LB_2 PB_13
+
+//  Right button
+#define RB_1 PB_1
+#define RB_2 PB_15
+
+//  JoyStick
+#define JOY_LEFT_X PA_0
+#define JOY_LEFT_Y PA_1
+#define JOY_RIGHT_X PA_4
+#define JOY_RIGHT_Y PB_0
+
+//  Emergency stop
+#define EMERGENCY_STOP PA_13
+
+//  IM920
+#define IM920_BAUDRATE 115200
+#define IM920_TX PA_15
+#define IM920_RX PB_7
+#define IM920_XMIT PH_1
+#define IM920_SLEEP PC_2
+#define IM920_STATUS PC_3
+#define IM920_BUSY PC_0
+#define IM920_RESET PC_1
+
+//  LED
+#define LED_1 PB_9
+#define LED_2 PB_8
+#define LED_3 PC_9
+#define LED_4 PC_8
+#define LED_5 PC_6
+
+#define JOYSTIC_DEADZONE 0.05
+
+#define SHOOTER_POWER_FAST_DIFFERENCE 0.10
+#define SHOOTER_POWER_SLOW_DIFFERENCE 0.01
+
+#define LOOP_RATE 100
+#define COOLTIME_MS 250
+
+////////////////////////////////////
+//  Private Type Definitio
+
+typedef struct ShooterType
+{
+    //  shooter num
+    int8_t num;
+    //  Range 0-1
+    float power;
+    /**
+     *  0: nothing
+     *  1: up
+     *  2: down
+     *  3: shoot
+     */
+    int8_t action;
+} shooter_t;
+
+typedef struct Vector3Type
+{
+    float x;
+    float y;
+    float z;
+} vector3_t;
+
+/**
+ * @brief Native Controller (what is unformatted joy controller variables)
+ */
+typedef struct ControllerType
+{
+    //  movement mode
+    int8_t movement_mode;
+    //  include vector-x,y,theta
+    vector3_t movement;
+    //  shooter variable
+    bool all_reload;
+    shooter_t shooter;
+} controller_t;
+
+////////////////////////////////////
+//  Private Variable
+//  LC Interrupt
+InterruptIn lc_up(LC_UP, PullDown);
+InterruptIn lc_down(LC_DOWN, PullDown);
+
+//  RC Interrupt
+InterruptIn rc_left(RC_LEFT, PullDown);
+InterruptIn rc_right(RC_RIGHT, PullDown);
+//  RC DigitalIn
+DigitalIn rc_up(RC_UP, PullDown);
+DigitalIn rc_down(RC_DOWN, PullDown);
+
+//  LB DigitalIn
+DigitalIn lb_1(LB_1, PullDown);
+
+//  RB Interrupt
+InterruptIn rb_1(RB_1, PullDown);
+InterruptIn rb_2(RB_2, PullDown);
+
+//  JoyStick
+AnalogIn joy_x(JOY_LEFT_X);
+AnalogIn joy_y(JOY_LEFT_Y);
+AnalogIn joy_theta(JOY_RIGHT_X);
+
+//  UART
+UnbufferedSerial dev(IM920_TX, IM920_RX, IM920_BAUDRATE);
+
+//  Intruppt timer
+Timer interrupt_timer;
+
+//  Internal variables
+//  controller
+controller_t _controller = {
+    0,
+    {
+        0.0,
+        0.0,
+        0.0
+    },
+    false,
+    {
+        0,
+        0.0,
+        0
+    }
+};
+
+////////////////////////////////////
+//  Private Prototype Function
+//  Initializer callback
+static void initialize_callback();
+//  Transfer packet
+void transfer_packet();
+
+//  Update controller
+void update_controller();
+
+//  insert filltered value into controller type
+void fillter_analog_value();
+
+//  LC interrupt
+void lc_up_callback();
+void lc_down_callback();
+
+//  RC interrupt
+void rc_left_callback();
+void rc_right_callback();
+
+//  RB interrupt
+void rb_1_callback();
+void rb_2_callback();
+
+//  timer
+bool is_cooltime();
+void reset_cooltime();
+
+
+int main()
+{
+    printf("Launch\n\r");
+    interrupt_timer.start();
+
+    initialize_callback();
+
+    while (true) {
+
+        //  update controller
+        update_controller();
+
+        //  publish
+        transfer_packet();
+
+        wait_us(LOOP_RATE * 1000);
+    }
+}
+
+static void initialize_callback()
+{
+    //  lc
+    lc_up.rise(lc_up_callback);
+    lc_down.rise(lc_down_callback);
+    //  rc
+    rc_left.rise(rc_left_callback);
+    rc_right.rise(rc_right_callback);
+    //  rb
+    rb_1.rise(rb_1_callback);
+    rb_2.rise(rb_2_callback);
+}
+
+//  Transfer packet
+void transfer_packet()
+{
+    //  TODO create UART system with im920sl
+
+    //  DEBUG
+    // printf(
+    //     "m:%d\n\rX:%.3f Y:%.3f T:%.3f\n\ra:%d\n\rn:%d p:%.3f ac:%d\n\n\r",
+    //     _controller.movement_mode,
+    //     _controller.movement.x, _controller.movement.y, _controller.movement.z,
+    //     _controller.all_reload,
+    //     _controller.shooter.num, _controller.shooter.power, _controller.shooter.action
+    // );
+    printf(
+        "m:%d a:%d n:%d p:%.3f ac:%d\n\r",
+        _controller.movement_mode,
+        _controller.all_reload,
+        _controller.shooter.num, _controller.shooter.power, _controller.shooter.action
+    );
+}
+
+//  update controller
+void update_controller()
+{
+    //  update joystick
+    fillter_analog_value();
+
+    //  shooter action
+    if(_controller.shooter.action != 3)
+    {
+      if(rc_up && rc_down)
+      {
+        //  No! No! Don't press the combination of UP + DOWN
+      }
+      else if (rc_up)
+      {
+        _controller.shooter.action = 1;
+      }
+      else if (rc_down)
+      {
+        _controller.shooter.action = 2;
+      }
+    }
+}
+
+//  insert filltered value into controller type
+void fillter_analog_value()
+{
+    //  Joy
+    float x = (joy_x.read() - 0.5) * 2;
+    float y = (joy_y.read() - 0.5) * 2;
+    float theta = (joy_theta.read() - 0.5) * 2;
+
+    //  deadzone
+    _controller.movement.x = -JOYSTIC_DEADZONE < x && x < JOYSTIC_DEADZONE ? 0.0 : x;
+    _controller.movement.y = -JOYSTIC_DEADZONE < y && y < JOYSTIC_DEADZONE ? 0.0 : y;
+    _controller.movement.z = -JOYSTIC_DEADZONE < theta && theta < JOYSTIC_DEADZONE ? 0.0 : theta;
+}
+
+//  LC interrupt
+void lc_up_callback()
+{
+    if(is_cooltime())
+    {
+        return;
+    }
+
+    float power = _controller.shooter.power;
+
+    //  increase
+    power += lb_1 ? SHOOTER_POWER_FAST_DIFFERENCE: SHOOTER_POWER_SLOW_DIFFERENCE;
+
+    //  limit
+    _controller.shooter.power = power > 1.0 ? 1.0: power;
+
+    reset_cooltime();
+}
+
+void lc_down_callback()
+{
+    if(is_cooltime())
+    {
+        return;
+    }
+
+    float power = _controller.shooter.power;
+
+    //  increase
+    power -= lb_1 ? SHOOTER_POWER_FAST_DIFFERENCE: SHOOTER_POWER_SLOW_DIFFERENCE;
+
+    //  limit
+    _controller.shooter.power = power < 0.0 ? 0.0: power;
+
+    reset_cooltime();
+}
+
+//  RC interrupt
+void rc_left_callback()
+{
+    if(is_cooltime())
+    {
+        return;
+    }
+
+    _controller.all_reload = true;
+
+    //  publish
+    transfer_packet();
+    //  reset
+    _controller.all_reload = false;
+
+    reset_cooltime();
+}
+
+void rc_right_callback()
+{
+    if(is_cooltime())
+    {
+        return;
+    }
+
+    int8_t mode = _controller.movement_mode + 1;
+
+    //  limitter
+    _controller.movement_mode = mode >= 3 ? 0: mode;
+
+    reset_cooltime();
+}
+
+//  RB interrupt
+void rb_1_callback()
+{
+    if(is_cooltime())
+    {
+        return;
+    }
+
+    int mode = _controller.shooter.num + 1;
+
+    //  limitter
+    _controller.shooter.num = mode >= 3 ? 0: mode;
+
+    reset_cooltime();
+}
+
+void rb_2_callback()
+{
+    if(is_cooltime())
+    {
+        return;
+    }
+
+    _controller.shooter.action = 3;
+    
+    //  publish
+    transfer_packet();
+    //  reset
+    _controller.shooter.action = 0;
+
+    reset_cooltime();
+}
+
+bool is_cooltime()
+{
+    return interrupt_timer.read_ms() < COOLTIME_MS;
+}
+
+void reset_cooltime()
+{
+    interrupt_timer.reset();
+}
